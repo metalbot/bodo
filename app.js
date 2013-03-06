83091f31cc3cd4366d642325fca3425a17ed02fb
Sean Schade <sean.schade@gmail.com>
2013-03-05 18:18:21 -0600

    #gklst organized external js libs

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/bootstrap/css'));
-app.use(express.static(__dirname + '/bootstrap/img'));
-app.use(express.static(__dirname + '/bootstrap/js'));
+app.use(express.static(__dirname + '/externs/jquery'))
+app.use(express.static(__dirname + '/externs/bootstrap/css'));
+app.use(express.static(__dirname + '/externs/bootstrap/img'));
+app.use(express.static(__dirname + '/externs/bootstrap/js'));
 
 //Heroku
 var port = process.env.PORT || 5000;
