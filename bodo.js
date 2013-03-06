249dc3b1ccc2d11be06da07d0185238096d9d932
Sean Schade <sean.schade@gmail.com>
2013-03-05 21:58:15 -0600

    #gklst added remove column button

--- bodo.js
+++ bodo.js
@@ -2,3 +2,8 @@ function addColumn(){
   $('.col:first').clone().appendTo('.bodo');
   $('.container').shapeshift(); 
 }
+
+function removeColumn(){
+  $('.col:last').remove();
+  $('.container').shapeshift();
+}
