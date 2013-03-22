6ab183ff681640e6db119c7ea7b5069fcec84ea2
Sean Schade <sean.schade@gmail.com>
2013-03-21 23:26:43 -0500

    #gklst updated express static

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/externs/jquery'))
-app.use(express.static(__dirname + '/externs/bootstrap/css'));
-app.use(express.static(__dirname + '/externs/bootstrap/img'));
-app.use(express.static(__dirname + '/externs/bootstrap/js'));
+//app.use(express.static(__dirname + '/externs/jquery'))
+//app.use(express.static(__dirname + '/externs/bootstrap/css'));
+//app.use(express.static(__dirname + '/externs/bootstrap/img'));
+//app.use(express.static(__dirname + '/externs/bootstrap/js'));
 
 //Heroku
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-app.get('/', function(req, res) {
-  app.use(express.static('bodo.html'));})
 
