00a4aae3e8428cc15f01d00d9346490536ed4c39
Sean Schade <sean.schade@gmail.com>
2013-03-23 23:18:36 -0500

    #gklst added jade template

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-//app.use(express.static(__dirname + '/externs/jquery'))
-//app.use(express.static(__dirname + '/externs/bootstrap/css'));
-//app.use(express.static(__dirname + '/externs/bootstrap/img'));
-//app.use(express.static(__dirname + '/externs/bootstrap/js'));
 
 //Heroku
 var port = process.env.PORT || 5000;
