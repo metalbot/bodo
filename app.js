c0ef70c85c4720158b4a86d8a6d08c25b5567a88
Sean Schade <sean.schade@gmail.com>
2013-03-24 00:05:50 -0500

    #gklst created jade template for index

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
 //Heroku
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
