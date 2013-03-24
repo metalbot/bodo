cd1b5d8a865c92ff4b9387bb5aedc5e4d7e32fef
Sean Schade <sean.schade@gmail.com>
2013-03-24 16:18:29 -0500

    #gklst applied well style to modal

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.use(express.static(__dirname));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-var card = {"story":"As a user, I want a basic card layout",
-           "tags":["card","story","tag","poc","layout","tasks"]};
+var card = {"tagLine":"Card Layout",
+            "story":"As a user, I want a basic card layout",
+            "tags":["card","story","tag","poc","layout","tasks"],
+            "acceptanceCriteria":["Show title","Show story","Show tags","Show acceptance criteria"]};
 
 app.get('/', function(req, res){
   res.render('index',card);
