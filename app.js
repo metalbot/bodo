774b02d6c89bbc402bfcbdec74cd2128c588f044
Sean Schade <sean.schade@gmail.com>
2013-03-24 10:45:37 -0500

    #gklst fixed json typo

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.use(express.static(__dirname));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-var card = {story:'As a user, I want a basic card layout'},
-           {tags:['card','story','tag','poc','layout','tasks']};
+var card = {"story":"As a user, I want a basic card layout"},
+           {"tags":["card","story","tag","poc","layout","tasks"]};
 
 app.get('/', function(req, res){
   res.render('index',card);
