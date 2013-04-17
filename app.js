467359013b69b694bc0d078b658689480d0c4535
Sean Schade <sean.schade@gmail.com>
2013-04-16 22:27:13 -0500

    #gklst added boards and cards back to res.render

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.post('/v0/cards', function(req, res){
 app.get('/', function(req, res){
 	getBoards(function(err, boards){
 		getCards(function(err, cards){
-			res.render('index');
+			res.render('index', {boards: boards, cards: cards});
 		});
 	});
 });
