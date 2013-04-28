1a8292f9395be7c3878bdda6c75dfa915a22c7e9
Sean Schade <sean.schade@gmail.com>
2013-04-28 18:19:47 -0500

    #gklst added client side service to get boards

--- public/js/controllers/BoardCtrl.js
+++ public/js/controllers/BoardCtrl.js
@@ -1,34 +1,6 @@
-function BoardCtrl($scope) {
-	$scope.board = {
-	   	"_id": {
-		        "$oid": "516b1ed7e4b03ed094d98458"							   
-				},
-			    "title": "Jidoka Research and Development Board",
-				    "phases": [
-					 {
-						"phase": "MMFs",
-						"title": "MMFs"
-					 },
-					 {
-						"phase": "technology_ideas",
-						"title": "technology ideas"
-					 },
-					 {
-						"phase": "understand_refine",
-						"title": "understand & refine"
-					 },
-					 {
-						"phase": "commit",
-						"title": "commit"
-					 },
-					 {
-						"phase": "do_build_verify",
-						"title": "do/build & verify"
-					 },
-					 {
-						"phase": "observe_learn",
-						"title": "observe & learn"
-					}
-			    ]
-		}
-	};
+function BoardCtrl($scope, $http) {
+	$http.get('/v0/boards').then(function(response) {
+		$scope.boards = response.data;
+		$scope.board = response.data[0];
+	});
+};
